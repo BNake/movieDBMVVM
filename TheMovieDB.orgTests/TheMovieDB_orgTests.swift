6b70bfb8299c17b652da2310f4d585bdcb4be6ce
Nazhmeddin <nazhmeddin@auto-nomad.kz>
2020-07-06 23:05:52 +0600

    Initial Commit

---
+++ TheMovieDB.orgTests/TheMovieDB_orgTests.swift
@@ -0,0 +1,34 @@
+//
+//  TheMovieDB_orgTests.swift
+//  TheMovieDB.orgTests
+//
+//  Created by Nazhmeddin Babakhanov on 06/07/2020.
+//  Copyright © 2020 Nazhmeddin Babakhanov. All rights reserved.
+//
+
+import XCTest
+@testable import TheMovieDB_org
+
+class TheMovieDB_orgTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
